f5057f9e267791b4618ceda4f33a9349dc2d184f
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2022-11-20 13:18:21 -0800

    all files added

---
+++ Ecommerce/website/views.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+from flask_mysqldb import MySQL
+
+views = Blueprint("views", __name__)
+
+@views.route("/")
+@views.route("/landpage")
+def landpage():
+    return render_template("landpage.html")
+
+@views.route("/testimonials")
+def testimonials():
+        return render_template("testimonials.html")
+
+@views.route("/aboutUs")
+def aboutUs():
+        return render_template("aboutUs.html")
+
